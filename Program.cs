c05943b8f667500efd6eedb5df076895400f1fc1
Quronanli <maximkabilenka@gmail.com>
2023-05-03 18:17:51 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+
+class Program
+
+{
+    static void Main(string[] args)
+    {
+        string sentence = "Це речення з багато пробілами між словами.  Тут є два пробіли підряд, але найбільше пробілів - це    чотири!";
+        int maxSpacesInARow = 0;
+        int spacesInARow = 0;
+        foreach (char c in sentence)
+        {
+            if (c == ' ')
+            {
+                spacesInARow++;
+            }
+
+            else
+            {
+                if (spacesInARow > maxSpacesInARow)
+                {
+                    maxSpacesInARow = spacesInARow;
+                }
+
+                spacesInARow = 0;
+            }
+        }
+        Console.WriteLine("Найбільша кількість пробілів, що розташовані підряд: " + maxSpacesInARow);
+    }
+}
